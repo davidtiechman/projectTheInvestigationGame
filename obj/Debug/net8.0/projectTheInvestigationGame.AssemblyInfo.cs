48c8601ff3656340a04adf736e75fa48c0e2b7e2
David Tiechman <d0534110171@gmail.com>
2025-06-15 15:42:08 +0300

    create readme

--- obj/Debug/net8.0/projectTheInvestigationGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/projectTheInvestigationGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectTheInvestigationGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2e197dfa103be3166cf64b3dd0a130d53eadef")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectTheInvestigationGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectTheInvestigationGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
